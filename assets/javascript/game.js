7fc7f493d71e96315bdb3b40e895e96859ccc560
thethatcher <thethatcher@gmail.com>
2017-06-26 18:51:07 -0600

    working javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,23 @@
-alert('success');
+var pressAnyKey = true;
+var newGameStart = true;
+var winCount = 0;
 
 document.onkeyup = function(event){
 	var key = event.key;
-	game.guessChar(key);
+	if(pressAnyKey){
+		game.setHtml(game.gameResultHtml,"");
+		pressAnyKey = false;
+		newGameStart = true;
+	}
+	if(newGameStart){
+		game.newGame();
+		newGameStart = false;
+	}
+	else{
+		game.guessChar(key);
+	}
+
+	
 }
 
 var game ={
@@ -10,13 +25,30 @@ var game ={
 	guessCountHtml: document.getElementById("guessCount"),
 	winCountHtml: document.getElementById("winCount"),
 	lettersGuessedHtml: document.getElementById("lettersGuessed"),
+	gameResultHtml: document.getElementById("gameResult"),
 	currentWord: "testing",
-	displayWord: "-------",
+	displayWord: "",
 	guessCount: 6,
-	winCount: 0,
 	lettersGuessed: [],
+	wordList: ["soccer","coach","player","goal","pitch","penalty","goalie"],
+
+	guessChar: function(char){
+		console.log("guess: " + char + " displayWord: " + this.displayWord);
+		if(this.guessCorrect(char)){
+			this.writeDisplayWord(char);
+		}
+		this.setHtml(this.displayWordHtml,this.displayWord);
+		if (this.gameOver()) {
+			pressAnyKey = true;
+		}
+		this.setHtml(this.displayWordHtml,this.displayWord);
+		this.setHtml(this.guessCountHtml,this.guessCount);
+		this.setHtml(this.lettersGuessedHtml,this.lettersGuessed);
+
+	},
 	setHtml: function(htmlElement,string){
-		this.htmlElement.innerText = string;
+		console.log("Updating HTML element" + htmlElement);
+		htmlElement.innerText = string;
 	},
 	guessCorrect: function(char){
 		if(this.currentWord.includes(char)){return true;}
@@ -36,6 +68,7 @@ var game ={
 				this.displayWord = this.replaceCharAt(this.displayWord,i,char);
 			}
 		}
+		console.log("New displayWord: " + this.displayWord);
 	},
 	replaceCharAt: function(string, index, char){
 		var result = string.substr(0,index) + char + string.substr(index+1);
@@ -43,18 +76,48 @@ var game ={
 		return result;
 	},
 	gameOver: function(){
-		if (this.guessCount <= 0) { return true;}
+		if (this.guessCount <= 0) {
+			this.setHtml(this.gameResultHtml,"You Lost!\n Press any key to play again");
+			return true;
+		}
+		else if(this.checkWin()){
+			winCount++;
+			this.setHtml(this.winCountHtml,winCount);
+			this.setHtml(this.gameResultHtml,"WINNER!\n Press any key to play again");
+			return true;
+		}
 		else{return false;}
 	},
-	guessChar: function(char){
-		console.log("guess: " + char + " displayWord: " + this.displayWord);
-		if(this.guessCorrect(char)){
-			this.writeDisplayWord(char);
-		}
-		if (this.gameOver()) {
-			alert("Game Over") 
-		}
+	checkWin: function(){
+		if (this.displayWord===this.currentWord) {return true;}
+		else{return false;}
+	},
+	
+	//start new game functions
+	newGame: function(){
+		this.resetBoard();
+		console.log("Board reset\n currentWord: " + this.currentWord);
 		this.setHtml(this.displayWordHtml,this.displayWord);
+		this.setHtml(this.guessCountHtml,this.guessCount);
+		this.setHtml(this.lettersGuessedHtml,this.lettersGuessed);
+	},
+	pickWord: function(){
+		var string = this.wordList[Math.floor(Math.random()*this.wordList.length)]
+		return string;
+	},
+	resetBoard: function(){
+		this.currentWord = this.pickWord();
+		this.resetDisplayWord(this.currentWord);
+		this.guessCount = 6;
+		this.lettersGuessed = [];
+
+	},
+	resetDisplayWord: function(string){
+		this.displayWord = "";
+		for (var i = 0; i < string.length; i++) {
+			this.displayWord = this.displayWord + '-';
+		}
+		console.log("resetting displayWord- currentWord: " + this.currentWord + "displayWord: " + this.displayWord);
 	}
 }
 
